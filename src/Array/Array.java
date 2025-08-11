fd9399480d7014d054e2d460032d632378c1eb2b
Mido-26 <hamiduyusuph00@gmail.com>
2025-08-11 15:27:50 +0300

    Refactor Array class for improved readability and formatting

--- src/Array/Array.java
+++ src/Array/Array.java
@@ -1,34 +1,19 @@
 package Array;
 
 public class Array {
-    private int[] elements;
+private int[] elements;
 
-    public Array(int size) {
-        elements = new int[size];
-    }
-
-    public void print() {
-        for (int i = 0; i < elements.length; i++) {
-            System.out.print(elements[i] + " ");
-        }
-        System.out.println();
-    }
-
-    public void insert(int element) {
-        elements[elements.length - 1] = element;
-    }
-
-    public void removeAt(int index) {
-        if (index < 0 || index >= elements.length) {
-            System.out.println("Index out of bounds");
-            return;
-        }
-        // remove the element at the specified index
-        // then shift elements to the left
-        for (int i = index; i < elements.length - 1; i++) {
-            elements[i] = elements[i + 1];
-        }
-        
-    }
-
+public Array(int size) {
+elements = new int[size];
 }
+
+public void print() {
+for (int i = 0; i < elements.length; i++) { System.out.print(elements[i] + " " ); } System.out.println(); } public void
+    insert(int element) { elements[elements.length - 1]=element; } public void removeAt(int index) { if (index < 0 ||
+    index>= elements.length) {
+    System.out.println("Index out of bounds");
+    return;
+    }
+    // remove the element at the specified index
+    // then shift elements to the left
+    for (int i = index; i < elements.length - 1; i++) { elements[i]=elements[i + 1]; } } }
